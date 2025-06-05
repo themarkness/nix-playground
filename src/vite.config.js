e344f90143a64ed58b0d75dd3bc21f6c7b1a2634
themarkness <williamsmarka@gmail.com>
2025-06-05 10:33:28 +0100

    create a config for Vite

---
+++ src/vite.config.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Nix!</h1>
+      <p>This app was built and deployed using Nix</p>
+    </div>
+  )
+}
+
+export default App
